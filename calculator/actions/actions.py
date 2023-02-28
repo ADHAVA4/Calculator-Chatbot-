c7363c1a0c1f5ce75c78dc8b7fbbcadc50aaa61c
ADHAVA4 <80875101+ADHAVA4@users.noreply.github.com>
2023-02-28 17:52:48 +0530

    Update actions.py

--- calculator/actions/actions.py
+++ calculator/actions/actions.py
@@ -2,6 +2,27 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+class ActionGreetUser(Action):
+
+    def name(self) -> Text:
+        return "action_greet_user"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Check if the greeting has already been displayed in the conversation
+        if tracker.get_slot("greeting_displayed"):
+            return []
+
+        # Set the slot to indicate that the greeting has been displayed
+        tracker.slots["greeting_displayed"] = True
+
+        # Display the greeting message
+        dispatcher.utter_message(template="utter_greeting")
+
+        return []
+
 class ActionAddNumbers(Action):
 
     def name(self) -> Text:
